42914f3a5944c810cc07892afa3e17651b4d409d
ThomasRalston <164934510+ThomasRalston@users.noreply.github.com>
2024-04-15 17:25:01 -0400

    Update and rename conversationstest.java to conversations.txt

--- CS180_Phase1/src/conversationstest.java
+++
@@ -1,2 +0,0 @@
-conv123///user1,user2///msg123//user1//user2//2024-03-31 12:00:00//true//Hello, how are you?,msg124//user2//user1//2024-03-31 12:05:00//true//I'm fine, thank you!
-conv456///user3,user4///msg125//user3//user4//2024-03-31 12:10:00//true//Hey there!,msg126//user4//user3//2024-03-31 12:15:00//true//Hi! How can I help you?
