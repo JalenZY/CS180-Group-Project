c019ee669ebe86fb25b7eefcc9d3c1484b2374c4
ThomasRalston <164934510+ThomasRalston@users.noreply.github.com>
2024-04-15 16:50:49 -0400

    Update BlockedListInterface.java

--- CS180_Phase1/src/BlockedListInterface.java
+++ CS180_Phase1/src/BlockedListInterface.java
@@ -7,61 +7,40 @@ import java.util.Date;
  * Lists all the methods and their paramaters used by BlockedList.java
  *
  * @author ThomasRalton, L105
- * @version April 1, 2024
+ * @version April 15, 2024
  */
 public interface BlockedListInterface {
-    // Methods
+    // Method to compare the blocked list of a user
     ArrayList<String> blockedCompList(String username1);
 
+    // Method to check if two BlockedList objects are equal
     boolean equals(Object o);
-
-    // Setters
-    void setUser1ID(String user1ID);
-
-    void setUser2ID(String user2ID);
-
-    void setBlockedID(String blockedID);
-
-    void setStatus(String status);
-
-    void setSince(String since);
-
-    void setUserName1(String username1);
-
-    void setUserName2(String username2);
-
-    void setUser1FirstName(String user1FirstName);
-
-    void setUser1LastName(String user1LastName);
-
-    void setUser2FirstName(String user2FirstName);
-
-    void setUser2LastName(String user2LastName);
-
-    void setDate(Date date);
 
     // Getters
     String getUser1ID();
-
     String getUser2ID();
-
     String getBlockedID();
-
     String getStatus();
-
     String getSince();
-
     String getUserName1();
-
     String getUserName2();
-
     String getUser1FirstName();
-
     String getUser1LastName();
-
     String getUser2FirstName();
-
     String getUser2LastName();
+    String getDate();
 
-    Date getDate();
+    // Setters
+    void setUser1ID(String user1ID);
+    void setUser2ID(String user2ID);
+    void setBlockedID(String blockedID);
+    void setStatus(String status);
+    void setSince(String since);
+    void setUserName1(String username1);
+    void setUserName2(String username2);
+    void setUser1FirstName(String user1FirstName);
+    void setUser1LastName(String user1LastName);
+    void setUser2FirstName(String user2FirstName);
+    void setUser2LastName(String user2LastName);
+    void setDate(String date);
 }
