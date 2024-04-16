1d8b98ebd7b1fdef7c356b19006f81f624e04604
YZ0406 <157168877+YZ0406@users.noreply.github.com>
2024-04-16 10:19:10 +0800

    Update MyClient.java

--- CS180-Phase-2/MyClient.java
+++ CS180-Phase-2/MyClient.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class ReceiveDataThread extends Thread {
     private final Socket socket;
@@ -20,7 +21,7 @@ class ReceiveDataThread extends Thread {
                 System.out.println("Server reply: " + serverResponse);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error in receiving data from server: " + e.getMessage());
         }
     }
 }
@@ -40,72 +41,130 @@ public class MyClient {
             ReceiveDataThread rcvThread = new ReceiveDataThread(socket);
             rcvThread.start();
 
-            String userInput;
-            System.out.println("Type your username:");
-            userInput = stdIn.readLine();
-            String fromUserName = userInput;
-            userInput = "USERNAME###" + userInput;
-            out.println(userInput);
+            System.out.println("Enter your username:");
+            String fromUserName = stdIn.readLine();
+            out.println("USERNAME###" + fromUserName);
 
-            while (!userInput.equalsIgnoreCase("bye")) {
-                System.out.println("Choose an action:\n1. Send message\n2. Add friend\n3. Block user\n4. Unblock user\n5. Update profile\n6. Remove user\nType the number of the action:");
-                String actionType = stdIn.readLine();
-                String message;
+            String actionType;
+            do {
+                System.out.println("\nChoose an action:");
+                System.out.println("1. Add User");
+                System.out.println("2. Remove User");
+                System.out.println("3. Update User Profile");
+                System.out.println("4. Block User");
+                System.out.println("5. Unblock User");
+                System.out.println("6. Add Friend");
+                System.out.println("7. Accept Friendship");
+                System.out.println("8. Decline Friendship");
+                System.out.println("9. Remove Friend");
+                System.out.println("0. Exit");
+                System.out.println("Enter the number of your choice:");
+
+                actionType = stdIn.readLine();
                 switch (actionType) {
                     case "1":
-                        System.out.println("Send message to:");
-                        String toUserName = stdIn.readLine();
-                        System.out.println("Type a message (type 'bye' to quit):");
-                        message = stdIn.readLine();
-                        out.println("SENDMESSAGE" + "###" + fromUserName + "###" + toUserName + "###" + message);
-                        userInput = message;
+                        addUser(out, stdIn);
                         break;
                     case "2":
-                        System.out.println("Enter the username of the friend you want to add:");
-                        String friendUserName = stdIn.readLine();
-                        out.println("ADDFRIEND" + "###" + fromUserName + "###" + friendUserName);
-                        userInput = "ADDFRIEND";
+                        removeUser(out, fromUserName);
                         break;
                     case "3":
-                        System.out.println("Enter the username of the user you want to block:");
-                        String blockUserName = stdIn.readLine();
-                        out.println("BLOCKUSER" + "###" + fromUserName + "###" + blockUserName);
-                        userInput = "BLOCKUSER";
+                        updateUserProfile(out, fromUserName, stdIn);
                         break;
                     case "4":
-                        System.out.println("Enter the username of the user you want to unblock:");
-                        String unblockUserName = stdIn.readLine();
-                        out.println("UNBLOCKUSER" + "###" + fromUserName + "###" + unblockUserName);
-                        userInput = "UNBLOCKUSER";
+                        blockUnblockUser(out, fromUserName, stdIn, true);
                         break;
                     case "5":
-                        System.out.println("Enter the field you want to update (e.g., userName, userFirstName, userLastName):");
-                        String fieldToUpdate = stdIn.readLine();
-                        System.out.println("Enter the new value:");
-                        String newValue = stdIn.readLine();
-                        out.println("UPDATEPROFILE" + "###" + fromUserName + "###" + fieldToUpdate + "###" + newValue);
-                        userInput = "UPDATEPROFILE";
+                        blockUnblockUser(out, fromUserName, stdIn, false);
                         break;
                     case "6":
-                        System.out.println("Are you sure you want to remove your account? (Type 'yes' to confirm)");
-                        String confirmation = stdIn.readLine();
-                        if (confirmation.equalsIgnoreCase("yes")) {
-                            out.println("REMOVEUSER" + "###" + fromUserName);
-                            userInput = "REMOVEUSER";
-                        } else {
-                            userInput = "";
-                        }
+                        manageFriendship(out, fromUserName, stdIn, "ADDFRIEND");
+                        break;
+                    case "7":
+                        manageFriendship(out, fromUserName, stdIn, "ACCEPTFRIEND");
+                        break;
+                    case "8":
+                        manageFriendship(out, fromUserName, stdIn, "DENYFRIEND");
+                        break;
+                    case "9":
+                        manageFriendship(out, fromUserName, stdIn, "REMOVEFRIEND");
+                        break;
+                    case "0":
+                        System.out.println("Exiting program.");
                         break;
                     default:
-                        System.out.println("Invalid action. Please choose a valid action.");
-                        userInput = "";
+                        System.out.println("Invalid action. Please enter a valid number.");
                         break;
                 }
-            }
+            } while (!actionType.equals("0"));
+            
             rcvThread.interrupt();
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error connecting to server: " + e.getMessage());
         }
+    }
+
+    private static void addUser(PrintWriter out, BufferedReader stdIn) throws IOException {
+        System.out.println("Enter details for new user:");
+        System.out.print("Username: ");
+        String username = stdIn.readLine();
+        System.out.print("First Name: ");
+        String firstName = stdIn.readLine();
+        System.out.print("Last Name: ");
+        String lastName = stdIn.readLine();
+        System.out.print("Password: ");
+        String password = stdIn.readLine();
+        System.out.print("Birthday (YYYY-MM-DD): ");
+        String birthday = stdIn.readLine();
+        System.out.print("Gender: ");
+        String gender = stdIn.readLine();
+        System.out.print("Hobby 1: ");
+        String hobby1 = stdIn.readLine();
+        System.out.print("Hobby 2: ");
+        String hobby2 = stdIn.readLine();
+        System.out.print("Hobby 3: ");
+        String hobby3 = stdIn.readLine();
+        System.out.print("Hobby 4: ");
+        String hobby4 = stdIn.readLine();
+        System.out.print("Home Location: ");
+        String homeLocation = stdIn.readLine();
+        System.out.print("User's Home Region: ");
+        String usersHomeRegion = stdIn.readLine();
+        System.out.print("College Name: ");
+        String collegeName = stdIn.readLine();
+
+        out.println("ADDUSER###" + username + "###" + firstName + "###" + lastName + "###" + password + "###" +
+                    birthday + "###" + gender + "###" + hobby1 + "###" + hobby2 + "###" + hobby3 + "###" + hobby4 +
+                    "###" + homeLocation + "###" + usersHomeRegion + "###" + collegeName);
+    }
+
+    private static void removeUser(PrintWriter out, String username) {
+        System.out.println("Are you sure you want to remove your account? (Type 'yes' to confirm)");
+        String confirmation = stdIn.readLine();
+        if (confirmation.equalsIgnoreCase("yes")) {
+            out.println("REMOVEUSER###" + username);
+        }
+    }
+
+    private static void updateUserProfile(PrintWriter out, String username, BufferedReader stdIn) throws IOException {
+        System.out.println("Enter the field you want to update:");
+        String fieldToUpdate = stdIn.readLine();
+        System.out.println("Enter the new value for " + fieldToUpdate + ":");
+        String newValue = stdIn.readLine();
+        out.println("UPDATEPROFILE###" + username + "###" + fieldToUpdate + "###" + newValue);
+    }
+
+    private static void blockUnblockUser(PrintWriter out, String username, BufferedReader stdIn, boolean block) throws IOException {
+        String action = block ? "BLOCKUSER" : "UNBLOCKUSER";
+        System.out.println("Enter the username of the user you want to " + (block ? "block:" : "unblock:"));
+        String targetUsername = stdIn.readLine();
+        out.println(action + "###" + username + "###" + targetUsername);
+    }
+
+    private static void manageFriendship(PrintWriter out, String username, BufferedReader stdIn, String action) throws IOException {
+        System.out.println("Enter the username of the user:");
+        String targetUsername = stdIn.readLine();
+        out.println(action + "###" + username + "###" + targetUsername);
     }
 }
