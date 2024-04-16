9bb1e94cdebeadf9e0863c5c5cb8b33f540ea3c9
YZ0406 <157168877+YZ0406@users.noreply.github.com>
2024-04-17 04:10:00 +0800

    Update MyClient.java

--- CS180-Phase-2/MyClient.java
+++ CS180-Phase-2/MyClient.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 class ReceiveDataThread extends Thread {
     private final Socket socket;
