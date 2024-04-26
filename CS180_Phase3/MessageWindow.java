a051545a1c35faba82efe9aa8e010ecd2257469b
ThomasRalston <164934510+ThomasRalston@users.noreply.github.com>
2024-04-26 11:27:01 -0400

    Update MessageWindow.java

--- CS180_Phase3/MessageWindow.java
+++ CS180_Phase3/MessageWindow.java
@@ -2,16 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-
+/**
+ * MessageWindow.java
+ *
+ * GUI Class that Allows for a user to send a message to another user
+ *
+ * @author ThomasRalston - Juan RodriguezL105
+ * @version April 26, 2024
+ */
 public class MessageWindow extends JFrame {
     private PrintWriter out;
     private BufferedReader in;
     private JTextField recipientField;
     private JTextArea messageArea;
+    public String fromUser;
 
-    public MessageWindow(PrintWriter out, BufferedReader in) {
+    public MessageWindow(PrintWriter out, BufferedReader in, String fromUser) {
         this.out = out;
         this.in = in;
+        this.fromUser = fromUser;
         setTitle("Message Window");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -27,19 +36,32 @@ public class MessageWindow extends JFrame {
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                sendMessage();
+                String recipient = recipientField.getText();
+                String message = messageArea.getText();
+                String messageStatus = "Error";
+                try {
+                    messageStatus = MyClient.manageSendMessage(out, in, fromUser, recipient, message);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                JOptionPane.showMessageDialog(MessageWindow.this, messageStatus);
+                clearMessageArea();
             }
         });
         add(sendButton, BorderLayout.SOUTH);
 
+        addWindowListener(new WindowAdapter() {
+            public void windowClosed(WindowEvent e) {
+                clearMessageArea();
+            }
+        });
+
         setVisible(true);
     }
 
-    private void sendMessage() {
-        String recipient = recipientField.getText();
-        String message = messageArea.getText();
-        // Send the message to the server
-        out.println("SENDMESSAGE###" + recipient + "###" + message);
+    // Method to clear the message area
+    private void clearMessageArea() {
+        messageArea.setText("");
     }
 }
 
