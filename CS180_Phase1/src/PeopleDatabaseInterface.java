4fde92815b47520aae665689b03caa1cd265d006
ThomasRalston <164934510+ThomasRalston@users.noreply.github.com>
2024-04-26 10:53:35 -0400

    Update PeopleDatabaseInterface.java

--- CS180_Phase1/src/PeopleDatabaseInterface.java
+++ CS180_Phase1/src/PeopleDatabaseInterface.java
@@ -10,48 +10,24 @@ import java.util.Date;
  */
 public interface PeopleDatabaseInterface {
 
-    // Add a new user to the database
     boolean addUser(UserProfile user);
-
-    // Remove a user from the database
     boolean removeUser(String userID);
-
-    // Update a user's profile
     boolean updateUserProfile(String userID, String toBeUpdated, String updatedPart);
-
-    // Block a user
     boolean blockUser(String blockerID, String blockedID, String date);
-
-    // Unblock a user
     boolean unblockUser(String blockerID, String blockedID);
-
-    // Add a friend
     boolean addFriend(String user1ID, String user2ID, String date);
-
-    // Accept a friend request
     boolean acceptFriend(String user1ID, String user2ID, String date);
-
-    // Deny a friend request
-    boolean denyFriend(String user1ID, String user2ID);
-
-    // Remove a friend
+    boolean deniedFriend(String user1ID, String user2ID);
     boolean removeFriend(String user1ID, String user2ID);
-
-    // Check if a specific block exists between two users
     boolean checkSpecificBlock(String unBlockerID, String blockedID);
-
-    // Check if a block exists between two users
     boolean checkBlock(String user1ID, String user2ID);
-
-    // Check if two users are friends
     boolean checkFriend(String user1ID, String user2ID);
-
-    // Method to get userID given userName
+    boolean checkExistingFriend(String user1ID, String user2ID);
+    boolean checkSpecificFriend(String user1Name, String user2Name);
     String userNameToUserID(String userName);
-
-    // Helper Method that Reads Text Files as an ArrayList
-    ArrayList<String> readFileToArray(String filename);
-
-    // Method to rewrite text Files
-    void writeChangesToFile(String filename, ArrayList<String> updatedInfo);
+    String userIDToUserName(String userID);
+    String searchForUser(String textComponent);
+    boolean checkUser(String userName);
+    boolean userLogin(String userName, String password);
+    ArrayList<String> printFriends(String userID);
 }
